6b1f3c7b99eae6e4ecca3b79a932b095d8f4a59d
Gleb <gleb.kurchitskiy@gmail.com>
2024-10-27 20:19:31 +0300

    all object

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,41 +1,11 @@
-const products = [
-  {
-    img: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "  Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 45,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    img: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 40,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    img: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 45,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
 products.forEach((product) => {
-  const mainBlock = document.querySelector(".products-grid");
+  const mainBlock = document.querySelector(".js-products-grid");
   mainBlock.innerHTML += `
             <div class="product-container">
           <div class="product-image-container">
             <img
               class="product-image"
-              src="${product.img}"
+              src="${product.image}"
             />
           </div>
 
@@ -48,10 +18,14 @@ products.forEach((product) => {
               class="product-rating-stars"
               src="images/ratings/rating-${product.rating.stars}.png"
             />
-            <div class="product-rating-count link-primary">${product.rating.count}</div>
+            <div class="product-rating-count link-primary">${
+              product.rating.count
+            }</div>
           </div>
 
-          <div class="product-price">$${product.priceCents}</div>
+          <div class="product-price">$${(product.priceCents / 100).toFixed(
+            2
+          )}</div>
 
           <div class="product-quantity-container">
             <select>
